1f98608281a765a28ee390a1b32688d0ce155fa0
tycrek <josh.moore@jmoore.dev>
2019-10-01 10:53:03 -0600

    Added video loading functions

--- client/js/client.js
+++ client/js/client.js
@@ -2,8 +2,21 @@ var $ = require('jquery');
 
 console.log('hello');
 
+$(window).on('load', () => {
+	let video = $('#video');
+	video.on('click', () => {
+		if (video.get(0).paused) video.get(0).play();
+		else video.get(0).pause();
+	});
+});
+
 window.random = () => {
 	let min = $('#min').val();
 	min = min === '' ? 0 : min;
-	alert(min);
+	fetch(`/getVine/${min}`)
+		.then((res) => res.json())
+		.then((json) => {
+			$('#video').attr('src', json.videourl);
+			$('#video').load();
+		});
 }
