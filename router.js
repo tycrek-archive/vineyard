3ab73f456e53bb61b294f60be38e59f9d191cc56
tycrek <josh.moore@jmoore.dev>
2019-10-03 10:45:16 -0600

    Added comments

--- router.js
+++ router.js
@@ -8,33 +8,36 @@ function respond(res, payload, type = 'html', status = 200) {
 	res.send(payload);
 }
 
-// Check if we are sending the full DOM or a piece
-Router.use((req, res, next) => {
-	// CORS headers
+// Set CORS headers for fetch() API
+Router.use((_req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
 	next();
 });
 
-Router.get('/', (req, res) => {
+// Root index
+Router.get('/', (_req, res) => {
 	Frontend.testIndex().then((html) => respond(res, html));
 });
 
-Router.get('/js', (req, res) => {
+// Client-side JavaScript compiled by Browserify
+Router.get('/js', (_req, res) => {
 	Frontend.js().then((js) => respond(res, js, 'js'));
 });
 
+// SASS -> CSS compiled by node-sass
 Router.get('/css', (_req, res) => {
 	Frontend.css().then((css) => respond(res, css, 'css'));
 });
 
+// Return a random Vine with at least min loops
 Router.get('/getRandomVine/:min', (req, res) => {
 	let min = req.params.min;
 	Psql.getRandomVine(min)
 		.then((vine) => respond(res, vine, 'json'));
 });
 
+// Return a Vine with vineId
 Router.get('/getVine/:vineId', (req, res) => {
 	let vineId = req.params.vineId;
 	Psql.getVine(vineId)
