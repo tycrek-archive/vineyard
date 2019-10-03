84911871a6a346e77207d47cb6b9ccdf4411963a
tycrek <josh.moore@jmoore.dev>
2019-10-03 13:57:30 -0600

    Minor changes to starting the server

--- app.js
+++ app.js
@@ -13,17 +13,23 @@ app.use('/', Router);
 
 // Initialize and start the server
 Utils.init()
-	.then((server) => {
-		if (server.https) {
-			require('https').createServer({
-				key: '', // For key and cert, these values can be specific in config.json but must be passed as a data string
-				cert: '',
-			}, app).listen(server.port, () => console.log(`Server (HTTPS) hosted on: ${server.port}`))
-		} else {
-			app.listen(server.port, () => console.log(`Server hosted on: ${server.port}`));
-		}
-	})
+	.then((server) => startServer(server))
 	.catch((err) => console.error(err));
+
+/**
+ * Start the server
+ * @param {JSON} server JSON object with server config info
+ */
+function startServer(server) {
+	if (server.https) {
+		require('https').createServer({
+			key: '', // For key and cert, these values can be specific in config.json but must be passed as a data string
+			cert: '',
+		}, app).listen(server.port, () => console.log(`Server (HTTPS) hosted on: ${server.port}`))
+	} else {
+		app.listen(server.port, () => console.log(`Server hosted on: ${server.port}`));
+	}
+}
 
 // Using module.exports lets us import the server
 // into an existing Express app, most likely using
